215880ec49ddfa47324fba1f312ffcd4ddc20e5a
JeanCoutinho <jeancoutinhoo7@gmail.com>
2021-09-12 09:57:51 -0300

    Arquivos adicionados

---
+++ Arquivos/etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros : 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'José Maria',
+                partido: 'PPP',
+                fotos: [
+                    {url:'38111.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Joice da Silva',
+                partido: 'ABC',
+                fotos: [
+                    {url:'77222.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Maria Maria',
+                partido: 'DEF',
+                vice: 'Beltrano Lopes',
+                fotos: [
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url: '99_2.png', legenda: 'Vice-prefeito', small: true}
+                ]
+
+            },
+            {
+                numero: '84',
+                nome: 'Jorge Luz',
+                partido: 'BBB',
+                vice: 'Jairo Copolla',
+                fotos:[
+                    {url: '84.jpg', legenda: 'Prefeito'},
+                    {url: '84_2.png', legenda: 'Vice-Prefeito', small:true}
+
+                ]
+            },
+        ]
+    }
+];
